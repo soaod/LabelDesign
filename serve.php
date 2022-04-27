d26567b1236de064e67d4f0be142f23fdafc8eb7
Saud Saeed <soao_d@hotmail.com>
2022-04-27 07:20:01 +0300

    Update serve.php

--- serve.php
+++ serve.php
@@ -16,7 +16,7 @@ if ( isset($_GET['query']) ) {
 
    $query = $_GET['query'];
    $result = $connection->query($query);
-   if ( stripos($query, "update") == 0 ) {
+   if ( stripos($query, "update") !== false ) {
       if ($connection->query($query) === true) {
          die("Record updated successfully");
        } else {
